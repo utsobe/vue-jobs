3d33782dfedb4319076b55972ff43333b0a96f23
utsobe <mmhutsobe01@gmail.com>
2025-01-27 14:41:50 +0800

    setup tailwindcss and added navbar, hero component with syntax of vue props

--- src/App.vue
+++ src/App.vue
@@ -1,67 +1,9 @@
 <script setup>
-import { onMounted, ref } from "vue";
-
-const name = ref("John Doe");
-const status = ref("active");
-const tasks = ref(["Task One", "Task Two", "Task Three"]);
-const newTask = ref("");
-
-const toggleStatus = () => {
-  status.value =
-    status.value === "active"
-      ? "pending"
-      : status.value === "pending"
-      ? "inactive"
-      : "active";
-};
-
-const addTask = (e) => {
-  if (newTask.value.trim() !== "") {
-    tasks.value.push(newTask.value);
-    newTask.value = "";
-  }
-};
-
-const deleteTask = (index) => {
-  tasks.value.splice(index, 1);
-};
-
-onMounted(async () => {
-  try {
-    const res = await fetch("https://jsonplaceholder.typicode.com/todos");
-    const data = await res.json();
-    tasks.value = data.map((task) => task.title);
-  } catch (error) {
-    console.error("Error fetching data from API");
-    // console.error(error);
-  }
-});
+import Navbar from "./components/Navbar.vue";
+import Hero from "./components/Hero.vue";
 </script>
 
 <template>
-  <h1>{{ name }}</h1>
-  <p v-if="status === 'active'">User is active</p>
-  <p v-else-if="status === 'pending'">User is pending</p>
-  <p v-else>User is inactive</p>
-
-  <form @submit.prevent="addTask">
-    <label for="newTask">Add Task</label>
-    <input type="text" id="newTask" name="newTask" v-model="newTask" />
-    <button type="submit">Submit</button>
-  </form>
-
-  <h3>Tasks:</h3>
-  <ul>
-    <li v-for="(task, index) in tasks" :key="task">
-      <span>
-        {{ task }}
-      </span>
-      <button style="margin-left: 10px" @click="deleteTask(index)">x</button>
-    </li>
-  </ul>
-  <br />
-  <!-- <button v-on:click="toggleStatus">Change Status</button> -->
-  <button @click="toggleStatus">Change Status</button>
+  <Navbar />
+  <Hero title="Test Title" subTitle="Test Subtitle" />
 </template>
-
-<style scoped></style>
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { fileURLToPath, URL } from "node:url";
-
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [vue(), vueDevTools(), tailwindcss()],
   server: {
     port: 3000,
   },
